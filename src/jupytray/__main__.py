bc55ba2058e6139bf5c60ac0360aa6cedc405488
Tomas Fiers <tomas.fiers@gmail.com>
2020-10-15 21:33:28 +0100

    🔁REFACT __main__.py to tray_app.py

--- src/jupytray/__main__.py
+++ src/jupytray/__main__.py
@@ -1,34 +1,8 @@
-import sys
-from pathlib import Path
+"""
+This script is executed when running `python -m jupytray`.
+"""
 
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMenu, QSystemTrayIcon
-
-from .notebook_server import NotebookServer
+from jupytray.tray_app import run_app
 
 
-server = NotebookServer()
-server.start()
-
-app = QApplication(sys.argv)
-
-icon_filename = "jupyter.ico"
-icon_path = Path(__file__).parent / icon_filename
-tray_icon = QSystemTrayIcon(QIcon(str(icon_path)), parent=app)
-tray_icon.show()
-
-
-def quit():
-    server.stop()
-    app.quit()
-
-
-menu = QMenu()
-menu.addAction("Restart server").triggered.connect(server.restart)
-menu.addAction("Stop server").triggered.connect(server.stop)
-menu.addAction("Exit").triggered.connect(quit)
-tray_icon.setContextMenu(menu)
-
-exit_code = app.exec_()
-
-sys.exit(exit_code)
+run_app()
---
+++ src/jupytray/tray_app.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMenu, QSystemTrayIcon
+
+from .notebook_server import NotebookServer
+
+
+def run_app():
+    app = make_app()
+    exit_code = app.exec_()
+    sys.exit(exit_code)
+
+
+def make_app():
+    app = QApplication(sys.argv)
+    menu = make_menu(app)
+    make_tray_icon(app, menu)
+    return app
+
+
+def make_menu(app):
+    server = NotebookServer()
+    server.start()
+
+    def quit():
+        server.stop()
+        app.quit()
+
+    menu = QMenu()
+    menu.addAction("Restart server").triggered.connect(server.restart)
+    menu.addAction("Stop server").triggered.connect(server.stop)
+    menu.addAction("Exit").triggered.connect(quit)
+    return menu
+
+
+def make_tray_icon(app, menu):
+    icon_path = Path(__file__).parent / "jupyter.ico"
+    tray_icon = QSystemTrayIcon(QIcon(str(icon_path)), parent=app)
+    tray_icon.setContextMenu(menu)
+    tray_icon.show()
