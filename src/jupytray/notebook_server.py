56ff9f273433b4cac78271f3ad106fc9d4f150ad
Tomas Fiers <tomas.fiers@gmail.com>
2020-10-22 21:19:53 +0200

    ⬆IMPR Popen → QProcess

--- src/jupytray/notebook_server.py
+++ src/jupytray/notebook_server.py
@@ -1,24 +1,25 @@
-from subprocess import Popen
+from PyQt5.QtCore import QProcess
 
 from jupytray.settings.storage import settings
 
 
 class NotebookServer:
 
-    process = None
+    process: QProcess = None
 
     def start(self):
         if self.process is None:
-            command = [
-                "pythonw",
+            program = "pythonw"
+            arguments = [
                 "-m",
                 "notebook",
                 f"--notebook-dir={settings.jupyter_root_dir}",
             ]
             if settings.open_browser == False:
-                command.append("--no-browser")
-            self.process = Popen(command)
-
+                arguments.append("--no-browser")
+            self.process = QProcess()
+            self.process.start(program, arguments)
+            
     def stop(self):
         if self.process is not None:
             self.process.terminate()
