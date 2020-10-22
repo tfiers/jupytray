de57aa7741578d9de22f6cb192bd5252f4730ec4
Tomas Fiers <tomas.fiers@gmail.com>
2020-10-22 22:20:49 +0200

    🐛FIX server terminate → kill (& refactor w/ new __init__)

--- src/jupytray/notebook_server.py
+++ src/jupytray/notebook_server.py
@@ -5,26 +5,23 @@ from jupytray.settings.storage import settings
 
 class NotebookServer:
 
-    process: QProcess = None
+    def __init__(self):
+        self.process = QProcess()
 
     def start(self):
-        if self.process is None:
-            program = "pythonw"
-            arguments = [
-                "-m",
-                "notebook",
-                f"--notebook-dir={settings.jupyter_root_dir}",
-            ]
-            if settings.open_browser == False:
-                arguments.append("--no-browser")
-            self.process = QProcess()
-            self.process.start(program, arguments)
+        program = "pythonw"
+        arguments = [
+            "-m",
+            "notebook",
+            f"--notebook-dir={settings.jupyter_root_dir}",
+        ]
+        if settings.open_browser == False:
+            arguments.append("--no-browser")
+        self.process.start(program, arguments)
 
     def stop(self):
-        if self.process is not None:
-            self.process.terminate()
-            self.process.waitForFinished()
-            self.process = None
+        self.process.kill()
+        self.process.waitForFinished()
 
     def restart(self):
         self.stop()
